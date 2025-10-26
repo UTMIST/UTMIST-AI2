797584d1d79f58e9db7c926e7e4725339c6381d7
IDontKnow64 <tomtompikachu@gmail.com>
2025-10-25 22:46:02 -0400

    Removed random lines

--- user/pvp_match.py
+++ user/pvp_match.py
@@ -1,28 +1,30 @@
 # import skvideo
 # import skvideo.io
 from environment.environment import RenderMode
-from environment.agent import SB3Agent, CameraResolution, RecurrentPPOAgent, BasedAgent, UserInputAgent, ConstantAgent, run_match, run_real_time_match, gen_reward_manager
-from user.my_agent import SubmittedAgent, ConstantAgent
+from environment.agent import SB3Agent, CameraResolution, RecurrentPPOAgent, BasedAgent, UserInputAgent, ConstantAgent, run_match, run_real_time_match
+from user.my_agent import SubmittedAgent
 
-reward_manager = gen_reward_manager()
+if __name__ == '__main__':
 
-experiment_dir_1 = "experiment_6/" #input('Model experiment directory name (e.g. experiment_1): ')
-model_name_1 = "rl_model00_steps" #input('Name of first model (e.g. rl_model_100_steps): ')
+    # reward_manager = gen_reward_manager()
 
-my_agent = UserInputAgent()
-#opponent = SubmittedAgent(None)
-opponent = ConstantAgent()
-# my_agent = UserInputAgent()
-# opponent = ConstantAgent()
+    # experiment_dir_1 = "experiment_6/" #input('Model experiment directory name (e.g. experiment_1): ')
+    # model_name_1 = "rl_model00_steps" #input('Name of first model (e.g. rl_model_100_steps): ')
 
-num_matches = 2 #int(input('Number of matches: '))
-#opponent=BasedAgent()
-match_time = 50000000000
-# 270
-# Run a single real-time match
-run_real_time_match(
-    agent_1=my_agent,
-    agent_2=opponent,
-    max_timesteps=30 * 999990000,  # Match time in frames (adjust as needed)
-    resolution=CameraResolution.LOW,
-)
+    my_agent = UserInputAgent()
+    #opponent = SubmittedAgent(None)
+    opponent = ConstantAgent()
+    # my_agent = UserInputAgent()
+    # opponent = ConstantAgent()
+
+    num_matches = 2 #int(input('Number of matches: '))
+    #opponent=BasedAgent()
+    match_time = 50000000000
+    # 270
+    # Run a single real-time match
+    run_real_time_match(
+        agent_1=my_agent,
+        agent_2=opponent,
+        max_timesteps=30 * 999990000,  # Match time in frames (adjust as needed)
+        resolution=CameraResolution.LOW,
+    )
